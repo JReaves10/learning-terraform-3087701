fa3334461279e5561237dceb4dcd7fa0424289d4
Jordan Reaves <J_C_Reaves@hotmail.com>
2025-05-08 10:09:47 -0400

    change the main

--- variables.tf
+++ variables.tf
@@ -3,7 +3,7 @@ variable "instance_type" {
   default     = "t3.nano"
 }
 
-variable "ami_filter" {
+variable "ami_filters" {
   description = "Name filter and owner for AMI"
 
   type    = object ({
